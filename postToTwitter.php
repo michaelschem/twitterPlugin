658cb49d8b66e1427a6aed52561619485780c703
michaelschem <michaelfschem@gmail.com>
2013-04-17 14:57:36 -0500

    Update postToTwitter.php

--- postToTwitter.php
+++ postToTwitter.php
@@ -7,10 +7,10 @@ if (isset($_POST['tweet'])) {
     require_once('twitteroauth/twitteroauth.php');
 
 // Twitter Connection Info
-    $twitter_access_token = '757961804-DV0VzERyFc42AJ9Ofmc9GtpwDT7fG2IUXixBR8jm';
-    $twitter_access_token_secret = 'jmqx7MEGYS5aJkiL1iI1qpvzBEo4My7ZgF4iaYOLaI4';
-    $twitter_consumer_key = 'tYRvTlOd8gQLdYS3EqxVPw';
-    $twitter_consumer_secret = 'rzuu6U1uEjfYr8YLJ5LpMDARFzINx0kDVBh2b07h8';
+    $twitter_access_token = 'REMOVED FOR SECURITY PURPOSES';
+    $twitter_access_token_secret = 'REMOVED FOR SECURITY PURPOSES';
+    $twitter_consumer_key = 'REMOVED FOR SECURITY PURPOSES';
+    $twitter_consumer_secret = 'REMOVED FOR SECURITY PURPOSES';
 
 // Connect to Twitter
     $connection = new TwitterOAuth($twitter_consumer_key, $twitter_consumer_secret, $twitter_access_token, $twitter_access_token_secret);
